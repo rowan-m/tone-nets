4d2ff7e13e8b585f8ad4cf600cade4065d5427e0
Rowan Merewood <rowan@merewood.org>
2026-05-11 17:47:49 +0100

    Fix permissions for GitHub Actions

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
+    reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default'],
     coverage: {
       enabled: true,
       provider: 'v8',

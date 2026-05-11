a96f25ba5e453ea27014235638f7df5bdf307a4e
Rowan Merewood <rowan@merewood.org>
2026-05-11 17:49:26 +0100

    Formatting

--- vitest.config.js
+++ vitest.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default'],
-    coverage: {
-      enabled: true,
-      provider: 'v8',
-      reporter: ['text', 'json', 'json-summary'],
-      reportsDirectory: './coverage',
-      reportOnFailure: true,
-      include: ['src/js/**/*.js'],
-      exclude: ['src/js/**/*.test.js'],
+    test: {
+        globals: true,
+        reporters: process.env.GITHUB_ACTIONS
+            ? ['default', 'github-actions']
+            : ['default'],
+        coverage: {
+            enabled: true,
+            provider: 'v8',
+            reporter: ['text', 'json', 'json-summary'],
+            reportsDirectory: './coverage',
+            reportOnFailure: true,
+            include: ['src/js/**/*.js'],
+            exclude: ['src/js/**/*.test.js'],
+        },
+        include: ['src/js/**/*.test.js'],
     },
-    include: ['src/js/**/*.test.js'],
-  },
 });
